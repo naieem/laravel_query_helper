7f466535b3f6bdd8ee2bde786ec8de98c3de577a
Naieem Mahmud Supto <naieemsupto@gmail.com>
2018-09-12 19:45:39 +0600

    query builder added and readme file updated

---
+++ Query_Helper.php
@@ -0,0 +1,321 @@
+<?php
+/**
+ * Created by Naieem Mahmud Supto.
+ * User: Naieem.mahmud
+ * Date: 9/6/2018
+ * Time: 3:06 PM
+ */
+
+/*
+
+        $whereClause = [
+                    "and" => [
+        //                "eql"=>[
+        //                    "product_title" => "Dolores mollitia."
+        //                ],
+        //                "gt" => [
+        //                    "product_price" => "47"
+        //                ]
+                    ],
+                    "or" => [
+        //                "eql"=>[
+        //                    "product_title" => "Dolores mollitia."
+        //                ],
+        //                "gt"=>[
+        //                    "product_price" => "47"
+        //                ]
+                    ]
+                ];
+        $orderBy = [
+            "product_price" => "desc"
+        ];
+        $pageNumber = 5;
+        $itemPerPage = 5;
+        $relationalFncs = ['metas', 'tags'];
+
+ */
+
+namespace App\QueryHelper;
+
+use Illuminate\Support\Facades\App;
+
+class QueryHelper
+{
+    private $className;
+    private $defaultItemsPerPage = 10;
+    private $conditionProperties = [
+        "eql" => "=",
+        "gt" => ">",
+        "lt" => "<",
+        "gte" => ">=",
+        "lte" => "<="
+    ];
+
+    /**
+     * Title: Construct functions
+     * Description: takes class reference to make it available for the closure
+     * @param abstruct class
+     * @return
+     */
+    public function __construct($class)
+    {
+        $this->className = app()->make($class);
+    }
+
+    /**
+     * Title: getAllData
+     * Description: Getting all data
+     * @param relational table functions
+     * @return returned collection of data
+     */
+    public function getAllData($relationFncs = null)
+    {
+        $datas = $this->className->all();
+        if (isset($relationFncs)) {
+            $datas = $this->relationalData($datas, $relationFncs);
+        }
+        return $datas;
+    }
+
+    /**
+     * Title:getByPagination
+     * Description: getting data by pagination
+     * @param perpageItem ,relational functions
+     * @return
+     */
+    public function getByPagination($resultPerPage, $relationFncs = null)
+    {
+        $datas = $this->className->paginate($resultPerPage);
+        if (count($relationFncs) > 0) {
+            $datas = $this->relationalData($datas, $relationFncs);
+        }
+        return $datas;
+    }
+
+    /**
+     * Title:getDataBySqlFilter
+     * Description: getting data by sql filter and query
+     * @param whereClause (array),Orderby(array),PageNumber(number),ItemPerPage(number),RelationalFunctions(string)
+     * @return collection
+     */
+    public function getDataBySqlFilter($whereClause = [], $orderBy = [], $pageNumber = null, $itemPerPage = null, $relationalFncs = null)
+    {
+        $query = $this->className->newQuery();
+        // -------------------------------------------
+        // block for extending where clause starts
+        // -------------------------------------------
+        if (isset($whereClause) && !empty($whereClause) && count($whereClause) > 0) {
+            $this->denormalizeWhereClause($query, $whereClause);
+        }
+        // -------------------------------------------
+        // block for extending where clause ends
+        // -------------------------------------------
+
+        // -------------------------------------------
+        // block for orderby clause starts
+        // ------------------------------------------
+        if (isset($orderBy) && !empty($orderBy)) {
+            $this->prepareOrderQUery($query, $orderBy);
+        }
+        // -------------------------------------------
+        // block for orderby clause ends
+        // ------------------------------------------
+
+        // -------------------------------------------
+        // block for pagination starts
+        // ------------------------------------------
+        if (isset($pageNumber) && isset($itemPerPage)) {
+            $this->preparePaginationQuery($query, $pageNumber, $itemPerPage);
+        } else {
+            $query->limit($this->defaultItemsPerPage);
+        }
+        // -------------------------------------------
+        // block for pagination ends
+        // ------------------------------------------
+
+        // --------------------------------------------
+        // block for getting relational datas starts
+        if (isset($relationalFncs) && !empty($relationalFncs)) {
+            $query->with($relationalFncs);
+        }
+        // --------------------------------------------
+        // block for getting relational datas ends
+        // --------------------------------------------
+        $datas = $query->get();
+
+        return [
+            "results" => $datas,
+            "totalCount" => count($datas)
+        ];
+    }
+
+    /**
+     * Title:getResultByJoin
+     * Description: getting result by join and others complex query
+     * @param parent table condition,child table condition, fields,orderby,pagenumber,item per page
+     * @return collection
+     */
+    public function getResultByComplex($parentCondition = null, $joiningcondition = null, $fields = null, $orderby = null, $pageNumber = null, $itemPerPage = null)
+    {
+        $query = $this->className->newQuery();
+        /* selecting field by raw query */
+        $query->selectRaw($fields);
+        // ----------------------------------------------------------
+        // block for joining condition denormalizing starts
+        // ----------------------------------------------------------
+        if (isset($joiningcondition) && !empty($joiningcondition) && count($joiningcondition)) {
+            $this->prepareJoinQuery($query, $joiningcondition);
+        }
+        // ----------------------------------------------------------
+        // block for joining condition denormalizing ends
+        // ----------------------------------------------------------
+
+        // ---------------------------------------------------------
+        // block for main table where clause denormalizing starts
+        // ---------------------------------------------------------
+        if (isset($parentCondition) && !empty($parentCondition) && count($parentCondition) > 0) {
+            $this->denormalizeWhereClause($query, $parentCondition);
+        }
+        // --------------------------------------------------------
+        // block for main table where clause denormalizing ends
+        // --------------------------------------------------------
+
+        // ------------------------------------------------
+        // block for orderby query preparation starts
+        // ------------------------------------------------
+        if (isset($orderby) && !empty($orderby)) {
+            $this->prepareOrderQUery($query, $orderby);
+        }
+        // ------------------------------------------------
+        // block for orderby query preparation ends
+        // ------------------------------------------------
+
+        // -------------------------------------------
+        // block for pagination starts
+        // ------------------------------------------
+        if (isset($pageNumber) && isset($itemPerPage)) {
+            $this->preparePaginationQuery($query, $pageNumber, $itemPerPage);
+        } else {
+            $query->limit($this->defaultItemsPerPage);
+        }
+        // -------------------------------------------
+        // block for pagination ends
+        // ------------------------------------------
+
+        $datas = $query->get();
+        return [
+            "results" => $datas,
+            "totalCount" => count($datas)
+        ];
+    }
+
+    /**
+     * Title:insert
+     * Description: insertin data in the table
+     * @param data object
+     * @return
+     */
+    public function insert($data)
+    {
+        return $this->className->create($data);
+    }
+
+    /**
+     * Title:prepareJoinQuery
+     * Description: preparing join query
+     * @param model ,conditions
+     * @return
+     */
+    public function prepareJoinQuery($query, $joiningcondition)
+    {
+        foreach ($joiningcondition as $key => $value) {
+            foreach ($value as $conditionKey => $conditionValue) {
+                $query->join($key, $conditionValue[0], $this->conditionProperties[$conditionKey], $conditionValue[1]);
+            }
+        }
+    }
+
+    /**
+     * Title:denormalizeWhereClause
+     * Description: Denormalizing where clause for preparing where conditional query
+     * @param query model, $whereClause
+     * @return
+     */
+    public function denormalizeWhereClause($query, $whereClause)
+    {
+        foreach ($whereClause as $key => $val) {
+            if (!empty($val) && count($val) > 0) {
+                foreach ($val as $andkey => $andval) {
+                    $this->prepareWhereQuery($query, $key, $andkey, $andval);
+                }
+            }
+        }
+
+    }
+
+    /**
+     * Title:preparePaginationQuery
+     * Description: preparing query for getting paginated data
+     * @param $queryModel (laravel query model),pagenumber,itemsperpage
+     * @return $queryModel
+     */
+    public function preparePaginationQuery($queryModel, $pageNumber, $itemPerPage)
+    {
+        $skippedItem = ($pageNumber - 1) * $itemPerPage;
+        $queryModel->skip($skippedItem);
+        $queryModel->take($itemPerPage);
+        return $queryModel;
+    }
+
+    /**
+     * Title:prepareWhereQuery
+     * Description:Preparing where clause
+     * @param $queryModel =['laravel model'],$queryType['and','or'],$conditionType ['eql','gt','gte' and etc],$conditionValues[$key=>$value pair]
+     * @return $queryModel
+     */
+    public function prepareWhereQuery($queryModel, $queryType, $conditionType = '', $conditionValues = [])
+    {
+
+        if ($queryType == "and") {
+            foreach ($conditionValues as $key => $val) {
+                $queryModel->where($key, $this->conditionProperties[$conditionType], $val);
+            }
+        }
+        if ($queryType == "or") {
+            foreach ($conditionValues as $key => $val) {
+                $queryModel->orWhere($key, $this->conditionProperties[$conditionType], $val);
+            }
+        }
+        return $queryModel;
+    }
+
+    /**
+     * Title: prepareOrderQUery
+     * Description: Preparing model for executing order query
+     * @param $queryModel =['laravel model'],$orderByProp[Order by property]
+     * @return $queryModel
+     */
+    public function prepareOrderQUery($queryModel, $orderByProp)
+    {
+        foreach ($orderByProp as $key => $val) {
+            $queryModel->orderBy($key, $val);
+        }
+        return $queryModel;
+    }
+
+    /**
+     * Title:relationalData
+     * Description: returning relational datas
+     * @param existing collection(datas), relational functions
+     * @return
+     */
+    public function relationalData($datas, $relationFncs)
+    {
+        foreach ($datas as $data) {
+            foreach ($relationFncs as $relationFnc) {
+                $data[$relationFnc];
+            }
+        }
+        return $datas;
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,228 @@
-# laravel_query_helper
+# Laravel Query Builder
+This is query builder which is acting as a helper for querying.
+
+# Using of QUery Builder:
+1. Create a folder in side your app folder and store the query builder there.
+2. Import it in your controller using use App\QueryHelper\QueryHelper; or whatever name you provide.
+3. In the constructor function initialize it by giving your corresponding model as paramer.It will create an instance of the given model.
+```php
+public function __construct()
+{
+     $this->helper = new QueryHelper(Product::class);
+}
+```
+
+4. Now call the functions anywhere in the controller function.Example:
+```php
+public function getByQuery(Request $request)
+{
+        $joiningClause = $request->input('joiningClause') ? $request->input('joiningClause') : null;
+        $whereClause = $request->has('whereClause') ? $request->input('whereClause') : null;
+        $orderBy = $request->input('orderBy') ? $request->input('orderBy') : null;
+        $fields = $request->input('selectedFields');
+        $products = $this->helper->getResultByComplex($whereClause, $joiningClause, $fields, $orderBy);
+        return response($products, 200);
+}
+```
+
+
+## Functions includes.
+
+### GetAllData()
+Params:
+1. Array of relational functions name:
+example:['tags','comments'].
+
+Response:
+Collection of all data coming from data base.
+
+### GetByPagination()
+
+Params:
+1. Result per page
+2. Array of relational functions name:
+example:['tags','comments'].
+
+Response:
+Collection of all data coming from data base.
+
+### GetResultByComplex()
+
+Params:
+1. whereClause: example.
+```php
+$whereClause = [
+            "and" => [
+                "eql" => [
+                    "metas.meta_name" => "Et."
+                ],
+                "eql" => [
+                    "metas.meta_value" => "Fugit."
+                ],
+                "eql" => [
+                    "products.product_stock" => "289"
+                ]
+            ],
+            "or" => [
+                "eql"=>[
+                    "product_title" => "Dolores mollitia."
+                ],
+                "gt"=>[
+                    "product_price" => "47"
+                ]
+            ]
+        ];
+```
+2. joiningClause. Example:
+```php
+$condition = [
+            'metas' => [
+                "eql" => [
+                    "metas.product_id",
+                    "products.id"
+                ]
+            ],
+            "productdenormalizes" => [
+                "eql" => [
+                    "productdenormalizes.product_id",
+                    "products.id"
+                ]
+            ]
+        ];
+```
+Here 'metas' and 'productdenormalizes' are the table with which 
+the main table is trying to join and get filtered data.
+
+3.Fields: Example:
+```php
+$fields = 'products.*,productdenormalizes.product_tag,metas.meta_name,metas.meta_value';
+```
+Here 'metas' and 'productdenormalizes' are the table with which 
+the main table is trying to join and get filtered data.
+
+4. OrderBy: exaple:
+```php
+$orderBy = [
+            "product_price" => "asc"
+];
+```
+
+5. PageNumber: exaple:
+```php
+$itemPerPage = 10;
+```
+
+6. ItemPerPage: exaple:
+```php
+$pageNumber = 2;
+        
+```
+
+Exaple of calling the Function:
+```php
+$this->helper->getResultByComplex($whereClause, $condition, $fields, $orderBy, $pageNumber, $itemPerPage);     
+```
+
+Response:
+Collection of all data coming from data base.
+
+### Insert()
+Params:
+1. Data of the model table. Example:
+
+```php
+public function store(Request $request)
+    {
+        $allData = $request->all();
+
+        try {
+            $insertResult = $this->helper->insert($allData);
+            return response($insertResult, 200);
+        } catch (BadRequestHttpException $e) {
+            return response($e, 500);
+        }
+    }     
+```
+
+Response:
+Collection of all data coming from data base.
+
+
+### Insert()
+Params:
+1. Data of the model table. Example:
+
+```php
+public function store(Request $request)
+    {
+        $allData = $request->all();
+
+        try {
+            $insertResult = $this->helper->insert($allData);
+            return response($insertResult, 200);
+        } catch (BadRequestHttpException $e) {
+            return response($e, 500);
+        }
+    }     
+```
+
+Response:
+Collection of all data coming from data base.
+
+### GetDataBySqlFilter()
+
+Params:
+1. whereClause: example.
+```php
+$whereClause = [
+            "and" => [
+                "eql" => [
+                    "metas.meta_name" => "Et."
+                ],
+                "eql" => [
+                    "metas.meta_value" => "Fugit."
+                ],
+                "eql" => [
+                    "products.product_stock" => "289"
+                ]
+            ],
+            "or" => [
+                "eql"=>[
+                    "product_title" => "Dolores mollitia."
+                ],
+                "gt"=>[
+                    "product_price" => "47"
+                ]
+            ]
+        ];
+```
+
+2. OrderBy: exaple:
+```php
+$orderBy = [
+            "product_price" => "asc"
+];
+```
+
+3. PageNumber: exaple:
+```php
+$itemPerPage = 10;
+```
+
+4. ItemPerPage: exaple:
+```php
+$pageNumber = 2; 
+```
+
+4. ItemPerPage: exaple:
+['tags','comments'].
+
+Exaple of calling the Function:
+```php
+$products = $this->helper->getDataBySqlFilter($whereClause, $orderBy, $pageNumber, $itemPerPage, $relationalFncs);     
+```
+
+Response:
+Collection of all data coming from data base.
+
+
